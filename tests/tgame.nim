ebdef428b33adff3f7c8f8152534035fa4aacffc
Mark Spanbroek <mark@spanbroek.net>
2019-11-26 09:46:35 +0100

    Remove superfluous tests

--- tests/tgame.nim
+++ tests/tgame.nim
@@ -3,12 +3,8 @@ import tables
 import game
 
 suite "setup":
-  test "it has a destroyer":
-    check ships.hasKey(destroyer)
-
   test "a destroyer has two empty positions":
     let ship = ships[destroyer]
-    check ship.len == 2
     check not isHit(ship, 0)
     check not isHit(ship, 1)
 
