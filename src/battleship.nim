943fee743f16156cca4bc786304f4c81546a283d
Mark Spanbroek <mark@spanbroek.net>
2019-11-26 09:41:37 +0100

    Add submarine et al.

--- src/battleship.nim
+++ src/battleship.nim
@@ -1,10 +1,15 @@
 import tables
 
 type
-  ShipType* = enum destroyer
+  ShipType* = enum destroyer, submarine, cruiser, battleship, carrier
   Ship* = seq[bool]
 
-const ships* = {destroyer: @[false, false]}.toTable
+const ships* = {
+  destroyer: @[false, false],
+  submarine: @[false, false, false],
+  cruiser:   @[false, false, false],
+  carrier:   @[false, false, false, false, false]
+}.toTable
 
 proc isHit*(ship: Ship, position: int): bool =
   ship[position]
--- tests/tbattleship.nim
+++ tests/tbattleship.nim
@@ -11,3 +11,11 @@ suite "setup":
     check ship.len == 2
     check not isHit(ship, 0)
     check not isHit(ship, 1)
+
+  test "there are five ships of correct length":
+    check ships[destroyer].len == 2
+    check ships[submarine].len == 3
+    check ships[cruiser].len == 3
+    # check ships[battleship].len == 4
+    check ships[carrier].len == 5
+
