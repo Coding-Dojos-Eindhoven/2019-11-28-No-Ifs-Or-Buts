1ef889cc760ebd74714fea0fb7c3526b3064b779
Mark Spanbroek <mark@spanbroek.net>
2019-11-26 09:31:04 +0100

    Extract isHit proc

--- src/battleship.nim
+++ src/battleship.nim
@@ -3,3 +3,5 @@ import tables
 type Ship* = enum destroyer
 
 const ships* = {destroyer: [false, false]}.toTable
+
+proc isHit*(position: bool): bool = position
--- tests/tbattleship.nim
+++ tests/tbattleship.nim
@@ -9,5 +9,5 @@ suite "setup":
   test "a destroyer has two empty positions":
     let ship = ships[destroyer]
     check ship.len == 2
-    check ship[0] == false
-    check ship[1] == false
+    check not isHit ship[0]
+    check not isHit ship[1]
