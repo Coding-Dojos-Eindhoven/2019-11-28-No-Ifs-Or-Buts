a4c6d919686152f1da0ddc659df0ce67020cfc6a
Mark Spanbroek <mark@spanbroek.net>
2019-11-26 12:02:50 +0100

    Convert tuple to named tuple

--- src/game.nim
+++ src/game.nim
@@ -4,7 +4,7 @@ import sequtils
 type
   Coordinate = (int, int)
   ShipType* = enum destroyer, submarine, cruiser, battleship, carrier
-  Ship* = seq[(Coordinate, bool)]
+  Ship* = seq[tuple[coordinate: Coordinate, hit: bool]]
   Ships* = Table[ShipType, Ship]
 
 const unplaced = ((0, 0), false)
@@ -17,22 +17,24 @@ const unplacedShips*: Ships = {
 }.toTable
 
 proc isHit*(ship: Ship, position: int): bool =
-  ship[position][1]
+  ship[position].hit
 
 proc place*(ships: Ships, ship: ShipType, coordinates: seq[Coordinate]): Ships =
   result = ships
   result[ship] = coordinates.mapIt((it, false))
 
 proc coordinates*(ship: Ship): seq[Coordinate] =
-  ship.mapIt(it[0])
+  ship.mapIt(it.coordinate)
 
 proc drop*(ships: Ships, row, column: int): Ships =
   result = ships
   for ship in low(ShipType)..high(ShipType):
-    result[ship] = result[ship].mapIt((it[0], it[1] or it[0] == (row, column)))
+    result[ship].applyIt(
+      (it.coordinate, it.hit or it.coordinate == (row, column))
+    )
 
 proc allHaveSunk*(ships: Ships): bool =
   result = true
   for ship in low(ShipType)..high(ShipType):
     for position in ships[ship]:
-      result = result and position[1]
+      result = result and position.hit
