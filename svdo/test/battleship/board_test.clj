5579954145a62e6fca761c62313b69045b350852
Stefan van den Oord <soord@mac.com>
2019-11-30 13:03:31 +0100

    Removes superfluous tests

--- svdo/test/battleship/board_test.clj
+++ svdo/test/battleship/board_test.clj
@@ -3,13 +3,6 @@
             [battleship.board :refer [empty-board place]]))
 
 (deftest board-test
-  (testing "the board is an empty map"
-    (is (map? empty-board)))
-
-  (testing "a destroyer can be placed on the board"
-    (let [updated (place empty-board :destroyer [])]
-      (is (some? (:destroyer updated)))))
-
   (testing "a ship is placed on given coordinates"
     (let [coordinates [[\a 3] [\a 4]]
           updated (place empty-board :destroyer coordinates)]
