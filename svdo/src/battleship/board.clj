fca588b6bf6f30f0ebd572daa89f2fad6dfea222
Stefan van den Oord <soord@mac.com>
2019-11-30 14:01:20 +0100

    Ships with empty coordinates are ignored

--- svdo/src/battleship/board.clj
+++ svdo/src/battleship/board.clj
@@ -6,7 +6,12 @@
   [board [ship-type coordinates]]
   (assoc board ship-type coordinates))
 
+(defn valid-coordinate? [[_ coordinates]]
+  (seq coordinates))
+
 ;; Technique used: [collection-instead-of-element]
 (defn place
   ([board ships]
-   (reduce place-single-ship board ships)))
+   (->> ships
+        (filter valid-coordinate?)
+        (reduce place-single-ship board))))
--- svdo/test/battleship/board_test.clj
+++ svdo/test/battleship/board_test.clj
@@ -2,20 +2,27 @@
   (:require [clojure.test :refer [deftest testing is]]
             [battleship.board :refer [empty-board place]]))
 
-(def coordinates [[\a 3] [\a 4]])
+(def destroyer-coordinates [[\a 3] [\a 4]])
+(def cruiser-coordinates [[\b 4] [\b 5] [\b 6]])
+(def battleship-coordinates [[\a 2] [\b 2] [\c 2] [\d 2]])
 
 (deftest board-test
   (testing "a destroyer is placed on given coordinates"
-    (let [updated (place empty-board {:destroyer coordinates})]
-      (is (= (:destroyer updated) coordinates))))
+    (let [updated (place empty-board {:destroyer destroyer-coordinates})]
+      (is (= (:destroyer updated) destroyer-coordinates))))
 
   (testing "another ship type can also be placed"
-    (let [updated (place empty-board {:cruiser coordinates})]
-      (is (= (:cruiser updated) coordinates))))
+    (let [updated (place empty-board {:cruiser cruiser-coordinates})]
+      (is (= (:cruiser updated) cruiser-coordinates))))
 
   (testing "multiple ships can be placed at once"
     (let [updated (-> empty-board
-                      (place {:destroyer []
-                              :battleship []}))]
+                      (place {:destroyer destroyer-coordinates
+                              :battleship battleship-coordinates}))]
       (is (and (some? (:destroyer updated))
-               (some? (:battleship updated)))))))
+               (some? (:battleship updated))))))
+
+  (testing "ships with empty coordinates are ignored"
+    (let [updated (-> empty-board
+                      (place {:destroyer []}))]
+      (is (= empty-board updated)))))
