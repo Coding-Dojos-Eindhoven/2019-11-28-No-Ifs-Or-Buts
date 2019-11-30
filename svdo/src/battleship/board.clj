cb67f64299ea30a37c1abdd363896b76af4954c1
Stefan van den Oord <soord@mac.com>
2019-11-30 12:58:08 +0100

    After calling `place` there is a ship on the board

--- svdo/src/battleship/board.clj
+++ svdo/src/battleship/board.clj
@@ -1,3 +1,6 @@
 (ns battleship.board)
 
 (def empty-board {})
+
+(defn place [board ship]
+  {:destroyer true})
--- svdo/test/battleship/board_test.clj
+++ svdo/test/battleship/board_test.clj
@@ -1,7 +1,11 @@
 (ns battleship.board-test
   (:require [clojure.test :refer :all]
-            [battleship.board :refer [empty-board]]))
+            [battleship.board :refer [empty-board place]]))
 
 (deftest board-test
   (testing "the board is an empty map"
-    (is (map? empty-board))))
+    (is (map? empty-board)))
+
+  (testing "a destroyer can be placed on the board"
+    (let [updated (place empty-board :destroyer)]
+      (is (some? (:destroyer updated))))))
